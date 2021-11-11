e4e64cb2c3eadddbdd225ed35a6879e53a697bc6
quaterhalfbro <eugeny.pakhalyuk@yandex.ru>
2021-11-11 13:08:12 +0300

    Add ransac in single class

--- LinearRegression.py
+++ LinearRegression.py
@@ -1,10 +1,11 @@
 import numpy as np
 
-class LinearRegression:    
+
+class LinearRegression:
     def __init__(self):
         self.weights = []
 
-    def fit(self, x: np.array, y: np.array, l2_regulation=0):
+    def fit(self, x: np.ndarray, y: np.ndarray, l2_regulation: int = 0):
         x_with_b = np.c_[np.ones(x.shape[0]), x]
         x_t = np.transpose(x_with_b)
         r_matrix = np.zeros([x_t.shape[0], x_t.shape[0]], int)
@@ -13,27 +14,20 @@ class LinearRegression:
             self.weights = np.dot(np.dot(np.linalg.inv(np.dot(x_t, x_with_b) + r_matrix), x_t), y)
         except np.linalg.LinAlgError:
             return -1
-        return self.score(x, y)
+        return 0
 
-    '''def gradient_fit(self, x: np.array, y: np.array, learning_rate, epochs_count):
-        self.weights = np.ones(x.shape[1])
-        for i in range(epochs_count):
-            gradient = 2 * np.dot(np.transpose(x), (np.dot(x, self.weights) - y))
-            self.weights -= learning_rate * gradient
-        return self.score(x, y)'''
-
-    def predict(self, x: np.array):
+    def predict(self, x: np.array) -> np.ndarray:
         x = np.c_[np.ones(x.shape[0]), x]
         return np.dot(x, self.weights)
 
-    def r(self, x: np.array, y: np.array):
+    def square_error(self, x: np.array, y: np.array) -> float:
         return np.sum((self.predict(x) - y) ** 2)
 
-    def score(self, x: np.array, y: np.array):
+    def score(self, x: np.array, y: np.array) -> float:
         y_mean = y.mean()
-        return 1 - self.r(x, y) / np.sum((y - y_mean) ** 2)
+        return 1 - self.square_error(x, y) / np.sum((y - y_mean) ** 2)
 
-    def cross_val_score(self, x: np.array, y: np.array, cv=5):
+    def cross_val_score(self, x: np.array, y: np.array, cv: int = 5) -> float:
         mean_score = 0
         for i in range(cv):
             train_x = np.delete(x, range(cv * i, cv * i + cv), axis=0)
@@ -47,10 +41,13 @@ class LinearRegression:
         mean_score /= cv
         return mean_score
 
-    def ransac_fit(self, x: np.array, y: np.array, radius=1):
-        self.fit(x, y)
+
+class RansacRegression(LinearRegression):
+    def fit(self, x: np.ndarray, y: np.ndarray, radius: float = 1) -> (float, np.ndarray):
+        super().fit(x, y)
         points = abs(self.predict(x) - y) < radius
         inlaers_x = x[points]
         inlaers_y = y[points]
-        score = self.fit(inlaers_x, inlaers_y)
-        return (score, points)
+        super().fit(inlaers_x, inlaers_y)
+        score = self.score(inlaers_x, inlaers_y)
+        return score, points
